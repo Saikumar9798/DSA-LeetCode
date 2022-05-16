81d029b17d9aacd253f825aba5a47832604a2f9e
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-16 18:21:35 +0530

    Time: 994 ms (9.09%), Space: 62.8 MB (9.09%) - LeetHub

---
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.ts
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function findMode(root: TreeNode | null): number[] {
+    let currentNodeVal = null, 
+        currentNodeCount = 0, 
+        maxCount = -Infinity, 
+        mode = [];
+    
+    const inOrder = root => {
+        if(!root) return;
+        
+        inOrder(root.left);
+        
+        if(currentNodeVal === root.val){
+            currentNodeCount++;
+        } else {
+            currentNodeCount = 1;
+            currentNodeVal = root.val;
+        }
+        if(currentNodeCount > maxCount){
+            maxCount = currentNodeCount;
+            mode = [root.val];
+        }
+        else if(currentNodeCount === maxCount){
+            mode.push(root.val)
+        }
+
+        console.log("currentNodeVal --> ", currentNodeVal)
+        console.log("currentNodeCount --> ", currentNodeCount)
+        console.log("maxCount --> ", maxCount)
+        console.log("mode --> ", mode)
+        console.log()
+
+        inOrder(root.right);
+    }
+    inOrder(root)
+   
+    return mode;
+};
