a3fdd936a15c7309be51f4b0f17718e8841c6574
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-19 20:07:33 +0530

    Time: 251 ms (70.13%), Space: 51.2 MB (16.88%) - LeetHub

--- 494-target-sum/494-target-sum.ts
+++ 494-target-sum/494-target-sum.ts
@@ -1,19 +1,3 @@
-// function findTargetSumWays(nums: number[], target: number): number {
-//   return dp(nums.length-1, nums, target)  
-// };
-
-// function dp(index: number,nums: number[], target: number, sum = 0, memo = new Map()){
-//     const key = `${index}#${sum}`;
-//     if (memo.has(key)) return memo.get(key);
-//     if(index < 0 && sum === target) return 1;
-//     if (index < 0) return 0;
-//     const positiveDp = dp(index-1, nums, target, sum+nums[index])
-//     const negativeDp = dp(index-1, nums, target, sum-nums[index])
-//     memo.set(key, sum);
-//     memo[key] = positiveDp+negativeDp
-//     return memo[key];
-// }
-
 var findTargetSumWays = function(nums, S) {
     const memo = new Map();
     const n = nums.length;
