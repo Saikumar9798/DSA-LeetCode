ca7a755247115122ebb9225ef3b13df20b6b0adc
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-20 11:23:42 +0530

    Time: 77 ms (68.71%), Space: 43 MB (52.47%) - LeetHub

--- 198-house-robber/198-house-robber.ts
+++ 198-house-robber/198-house-robber.ts
@@ -5,7 +5,7 @@ function rob(nums: number[]): number {
 function getMaxRob( nums: number[], index = nums.length-1, max = -Infinity, visited: any = {}){
     if(index in visited) return visited[index];
     if(index < 0) return null;
-    if (index === 0 || nums.length === 1) return nums[0];
+    if (index === 0) return nums[0];
     for(let i in nums){
             visited[i] = Math.max(nums[i]+getMaxRob(nums, +i-2, max, visited), getMaxRob(nums,+i-1, max, visited));
             if(visited[i] > max) max = visited[i]
