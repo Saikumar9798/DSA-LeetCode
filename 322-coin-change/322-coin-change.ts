bb282c79c213cb976bc6c5a937a64a282575b29c
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-21 15:54:12 +0530

    Time: 193 ms (58.93%), Space: 48.5 MB (63.99%) - LeetHub

--- 322-coin-change/322-coin-change.ts
+++ 322-coin-change/322-coin-change.ts
@@ -1,8 +1,4 @@
 function coinChange(coins: number[], amount: number): number {
-    return getMinCoins(coins, amount) === Infinity? -1 : getMinCoins(coins, amount);
-};
-
-function getMinCoins(coins,amount){
     const dpArray = new Array(amount+1).fill(Infinity);
     dpArray[0] = 0;
     for(let i = 1; i < dpArray.length; ++i){
@@ -11,5 +7,5 @@ function getMinCoins(coins,amount){
                 dpArray[i] = Math.min( 1+dpArray[i-num], dpArray[i]);
         })
     }
-    return dpArray[dpArray.length-1]
-}
+    return dpArray[dpArray.length-1] === Infinity? -1 : dpArray[dpArray.length-1];
+};
