2d7c479d4c57212a3930aae4aaae9951910b0af7
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-23 19:36:48 +0530

    Time: 2939 ms (5.71%), Space: 172.6 MB (5.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
@@ -3,7 +3,7 @@ function longestCommonSubsequence(text1: string, text2: string): number {
     function helper(i1: number, i2: number){
         const arg = `${i1}-${i2}`;
         if(dp[arg]) return dp[arg];
-        if(!text1 || !text2 || i1<0 || i2<0) {
+        if(i1<0 || i2<0) {
             dp[arg] = 0;
             return 0;
         }
