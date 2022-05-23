0c76aeacd5e0e7fa162f1035f318e477a403103b
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-23 18:19:59 +0530

    Time: 7710 ms (50.00%), Space: 174.4 MB (50.00%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.ts
+++ 474-ones-and-zeroes/474-ones-and-zeroes.ts
@@ -3,18 +3,18 @@ function findMaxForm(strs: string[], m: number, n: number): number {
     function getMaxForm(index: number, m: number, n: number){
         const arg = `${index}-${m}-${n}`
         if( dp[arg] ) return dp[arg]
-        if((index >= strs.length) || m < 0 || n < 0) {
+        if((index < 0) || m < 0 || n < 0) {
             dp[arg] = 0;
             return dp[arg] 
         }
         let considerCount = 0, skipCount = 0, [zero,one] = getOneZeroCount(strs[index]);
         if(m >= zero   && n >= one)
-            considerCount = 1+getMaxForm(index+1, m-zero, n-one);
-        skipCount = getMaxForm(index+1, m, n);
+            considerCount = 1+getMaxForm(index-1, m-zero, n-one);
+        skipCount = getMaxForm(index-1, m, n);
         dp[arg] =  Math.max(considerCount, skipCount)
         return  dp[arg]
     }
-    return getMaxForm(0, m, n)
+    return getMaxForm(strs.length-1, m, n)
 };
 
 function getOneZeroCount(num: string){
