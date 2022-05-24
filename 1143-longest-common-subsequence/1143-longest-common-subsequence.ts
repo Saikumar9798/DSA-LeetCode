7fc8448a1efef62dab432f1900c123153c2d0d1a
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-24 19:15:16 +0530

    Time: 106 ms (86.52%), Space: 53.9 MB (56.03%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.ts
@@ -1,18 +1,11 @@
-function longestCommonSubsequence(text1: string, text2: string): number {
-    let dp = {}
-    function helper(i1: number, i2: number){
-        const arg = `${i1}-${i2}`;
-        if(dp[arg]) return dp[arg];
-        if(i1<0 || i2<0) {
-            dp[arg] = 0;
-            return 0;
-        }
-        let count = 0;
-        if(text1[i1] === text2[i2]) count = 1+helper(i1-1, i2-1);
-        else
-            count = Math.max(helper(i1-1,i2), helper(i1,i2-1));
-        dp[arg] = count
-        return count;
-    }
-    return helper(text1.length-1, text2.length-1)
-};
+function longestCommonSubsequence(s1: string, s2: string): number{
+  let dp = new Array(s1.length+1).fill(0).map(x=>new Array(s2.length+1).fill(0))
+  for(let i=1; i<=s1.length; i++){
+      for(let j=1 ; j<=s2.length; j++){
+          if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1]+1
+          else
+              dp[i][j] = Math.max( dp[i-1][j] , dp[i][j-1] )
+      }
+  }
+    return dp[s1.length][s2.length]
+}
