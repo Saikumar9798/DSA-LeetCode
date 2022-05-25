0d0322a9277c24a8f174504b66cc7ef7d887b35d
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-25 18:10:17 +0530

    Time: 2097 ms (5.45%), Space: 46.6 MB (27.88%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.ts
+++ 45-jump-game-ii/45-jump-game-ii.ts
@@ -1,12 +1,11 @@
 function jump(nums: number[]): number {
-    let maxDistance = -Infinity, dp = {};
+    let dp = {};
     const numsLen = nums.length-1;
     function helper(i: number){
         if(i >= numsLen) return 0;
         if(dp[i]) return dp[i];
         let steps = Infinity;
             for(let j = i+1; j <= i+nums[i] && j <= numsLen; ++j){
-                maxDistance = Math.max(maxDistance, i+nums[i])
                 steps = Math.min(1+helper(j), steps);
             }
         dp[i] = steps;
