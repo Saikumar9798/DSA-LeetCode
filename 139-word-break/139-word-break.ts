e1d8f47849606739a6b51ebff51096ca9e1baa1c
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-27 11:39:47 +0530

    Time: 123 ms (30.04%), Space: 47.1 MB (19.01%) - LeetHub

---
+++ 139-word-break/139-word-break.ts
@@ -0,0 +1,36 @@
+function wordBreak(s: string, words: string[]): boolean {
+    let dp = Array(s.length+1).fill(false);
+    const newS = " "+s;
+    let wordsObject = {};
+    for(let i = 0 ; i < words.length; ++i)
+        wordsObject[`${words[i]}`] = words[i];
+    dp[0] = true;
+    for(let i =1; i <= newS.length; ++i){
+        for(let j = i-1; j > -1; --j){
+            if(dp[j] && (newS.slice(j+1, i+1) in wordsObject)){
+                dp[i] = true;
+                break;
+            }
+        }
+        console.log()
+    }
+    console.log(dp,wordsObject)
+    return dp[s.length]
+};
+
+// function wordBreak(s: string, words: string[]): boolean {
+//     let dp = {};
+//     function helper(i: number){
+//         if(i>=s.length) return true;
+//         if(dp[i]) return dp[i];
+//         for(let word of words){
+//             if(s.substr(i,word.length).includes(word)){
+//                 dp[i] = helper(i+word.length)
+//                 if(dp[i]) return true;
+//             }
+//         }
+//         dp[i] = false;
+//         return dp[i];
+//     }
+//     return helper(0);
+// };
