573a8e4ddc13900104f66efe21d5cf54df1976b5
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-29 20:16:44 +0530

    Time: 126 ms (17.31%), Space: 44.9 MB (44.23%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.ts
@@ -0,0 +1,28 @@
+function combinationSum4(nums: number[], target: number): number {
+    let dp = Array(target+1).fill(0);
+    dp[0] = 1;
+    for(let i = 1; i <= target; ++i){
+        for(let num of nums){
+            if(i - num >= 0) dp[i] += dp[i-num]
+        }
+    }
+    return dp[target];
+};
+
+// function combinationSum4(nums: number[], target: number): number {
+//     let count = 0;
+//     function helper(target: number): void{
+//         if(!target) {
+//             count++;
+//             return;
+//         }
+//         if(target < 0) return;
+//         for(let num of nums){
+//             if((target-num) >= 0) {
+//                 helper(target-num)
+//             }
+//         }
+//     }
+//     helper(target);
+//     return count;
+// };
