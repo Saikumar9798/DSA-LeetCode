070f012561ce94ef22e8c0949a28982ed481fbdf
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-31 12:09:20 +0530

    Time: 767 ms (57.59%), Space: 49.4 MB (43.11%) - LeetHub

---
+++ 79-word-search/79-word-search.ts
@@ -0,0 +1,46 @@
+function exist(board: string[][], word: string): boolean {
+  function isValidCell(m: number, n: number, i: number) {
+    if (
+      m >= board.length ||
+      n >= board[0].length ||
+      m < 0 ||
+      n < 0 ||
+      word[i] !== board[m][n] ||
+      board[m][n] === "."
+    )
+      return false;
+    return true;
+  }
+
+  function helper(m: number, n: number, i: number, search: string): boolean {
+    if (search === word) return true;
+    if (!isValidCell(m, n, i)) {
+      search = search.slice(0, search.length - 1);
+      return false;
+    }
+    const c = board[m][n];
+    board[m][n] = ".";
+    search += c;
+    const res =
+      helper(m - 1, n, i + 1, search) ||
+      helper(m, n + 1, i + 1, search) ||
+      helper(m + 1, n, i + 1, search) ||
+      helper(m, n - 1, i + 1, search);
+    board[m][n] = c;
+    return res;
+  }
+
+  let m = 0,
+    n = 0;
+  while (m < board.length) {
+    while (n < board[0].length) {
+      const helperRes = helper(m, n, 0, "");
+      if (helperRes) return true;
+      ++n;
+    }
+    n = 0;
+    ++m;
+  }
+
+  return false;
+}
