8d62eee412849282bc77cea2c04f06559f2a9936
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-05-31 17:23:19 +0530

    Time: 75 ms (87.64%), Space: 45.2 MB (68.73%) - LeetHub

--- 78-subsets/78-subsets.ts
+++ 78-subsets/78-subsets.ts
@@ -1,7 +1,7 @@
 function subsets(nums: number[]): number[][] {
     let result = [[]], stack = [];
     function helper(i: number){
-        if(i === nums.length) return null;
+        if(i === nums.length) return;
         while(i < nums.length){
             stack.push(nums[i]);
             result.push([...stack]);
