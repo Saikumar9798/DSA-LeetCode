98b77be6a8bde0c60b37c82929a331f65249a035
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-06-04 00:31:57 +0530

    Time: 782 ms (45.71%), Space: 84.4 MB (17.14%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.ts
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.ts
@@ -1,17 +1,30 @@
 class NumMatrix {
   matrix: number[][];
   constructor(matrix: number[][]) {
-    this.matrix = matrix;
-  }
-
-  sumRegion(row1: number, col1: number, row2: number, col2: number): number {
-    let sum = 0;
-    for (let i = row1; i <= row2; ++i) {
-      for (let j = col1; j <= col2; ++j) {
-        sum += this.matrix[i][j];
+    this.matrix = new Array(matrix.length)
+      .fill(0)
+      .map((_) => new Array(matrix[0].length).fill(0));
+    for (let i = 0; i < matrix.length; ++i) {
+      for (let j = 0; j < matrix[0].length; ++j) {
+        if (!i && !j) this.matrix[i][j] = matrix[i][j];
+        else if (!i) this.matrix[i][j] = matrix[i][j] + this.matrix[i][j - 1];
+        else if (!j) this.matrix[i][j] = matrix[i][j] + this.matrix[i - 1][j];
+        else
+          this.matrix[i][j] =
+            this.matrix[i - 1][j] +
+            this.matrix[i][j - 1] -
+            this.matrix[i - 1][j - 1] +
+            matrix[i][j];
       }
     }
-    return sum;
+  }
+  sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+    return (
+      this.matrix[row2][col2] -
+      (row1 - 1 >= 0 ? this.matrix[row1 - 1][col2] : 0) -
+      (col1 - 1 >= 0 ? this.matrix[row2][col1 - 1] : 0) +
+      (col1 - 1 >= 0 && row1 - 1 >= 0 ? this.matrix[row1 - 1][col1 - 1] : 0)
+    );
   }
 }
 
