1c88e10c9f5b3a73e4251b6fd783e503ae0134bb
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-06-04 22:27:07 +0530

    Time: 146 ms (25.00%), Space: 46 MB (60.94%) - LeetHub

---
+++ 51-n-queens/51-n-queens.ts
@@ -0,0 +1,48 @@
+function solveNQueens(n: number): string[][] {
+  const result: string[][] = [];
+  const resetMatrix = new Array(n).fill(0).map((_) => new Array(n).fill("."));
+  let queensMatrix = JSON.parse(JSON.stringify(resetMatrix));
+
+  function inRange(x: number, min: number, max: number) {
+    return (x - min) * (x - max) <= 0;
+  }
+
+  function validateQueen(row: number, col: number, i = 1) {
+    while (inRange(row - i, 0, n - 1)) {
+      if (queensMatrix[row - i][col] === "Q") return false;
+      ++i;
+    }
+    i = 1;
+    while (inRange(row - i, 0, n - 1) && inRange(col - i, 0, n - 1)) {
+      if (queensMatrix[row - i][col - i] === "Q") return false;
+      ++i;
+    }
+    i = 1;
+    while (inRange(row - i, 0, n - 1) && inRange(col + i, 0, n - 1)) {
+      if (queensMatrix[row - i][col + i] === "Q") return false;
+      ++i;
+    }
+    return true;
+  }
+  function helper(r: number, queensCount: number = 0): void {
+    if (queensCount === n) {
+      const nQueensPossibleSolution = [];
+      for (let arr of queensMatrix) {
+        nQueensPossibleSolution.push(arr.join(""));
+      }
+      result.push(nQueensPossibleSolution);
+      return;
+    }
+    for (let col = 0; col < n; ++col) {
+      if (validateQueen(r, col)) {
+        queensMatrix[r][col] = "Q";
+        ++queensCount;
+        helper(r + 1, queensCount);
+        queensMatrix[r][col] = ".";
+        --queensCount;
+      }
+    }
+  }
+  helper(0);
+  return result;
+}
