96789ec124134cece1ff79709ed09bdc78f4bedb
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-06-07 15:22:03 +0530

    Time: 98 ms (82.18%), Space: 51.3 MB (98.02%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.ts
@@ -1,10 +1,8 @@
 function maxProfit(prices: number[]): number {
     let min = prices[0], max = -Infinity;
     for(let i = 1; i < prices.length; ++i){
-        if(prices[i] < min){
+        if(prices[i] < min)
             min = prices[i];
-            continue;
-        }
         else{
             const maxLocal = Math.max(max, prices[i]-min);
             if(maxLocal > max) max = maxLocal
