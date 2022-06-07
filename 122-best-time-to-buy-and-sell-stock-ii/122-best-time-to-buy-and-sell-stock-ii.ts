f8f5bf12152aabb2176c461cfdd30b3439e4e636
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-06-07 19:07:52 +0530

    Time: 114 ms (26.59%), Space: 44 MB (60.71%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,7 @@
+function maxProfit(prices: number[]): number {
+    let profit = 0;
+    for(let i = 1; i < prices.length; ++i){
+        if(prices[i]-prices[i-1] > 0) profit += prices[i]-prices[i-1]
+    }
+    return profit
+};
