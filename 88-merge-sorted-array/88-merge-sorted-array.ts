fdde308eb3271506cee600b9e69e230acfcb7f15
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-06-07 11:47:57 +0530

    Time: 115 ms (16.27%), Space: 45 MB (12.95%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.ts
+++ 88-merge-sorted-array/88-merge-sorted-array.ts
@@ -3,11 +3,14 @@
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
   for (let i = m, j = 0; i < nums1.length; ++i, ++j) nums1[i] = nums2[j];
-  for (let i = 0, j = m; i <= m; ++i) {
-    if (nums1[i] > nums1[j]) {
-      [nums1[i], nums1[j]] = [nums1[j], nums1[i]];
-      const sorted = nums1.splice(j).sort((a, b) => a - b);
-      nums1.push(...sorted);
+    let gap = Math.ceil(nums1.length / 2);
+    while(true){
+        let i = 0, j = gap;
+        while(j < nums1.length){
+            if(nums1[i] > nums1[j]) [nums1[i], nums1[j]] = [nums1[j], nums1[i]];
+            ++i; ++j;
+        }
+        if(gap === 1) break;
+        gap = Math.ceil(gap/2);
     }
-  }
 }
