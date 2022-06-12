b0b25022907fe450a8a91b3b815ae0f8ffd99d3b
Venkata Saikumar Gollapudi <66771553+Saikumar9798@users.noreply.github.com>
2022-06-12 23:08:16 +0530

    Time: 544 ms (41.67%), Space: 67.7 MB (66.67%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.ts
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.ts
@@ -1,17 +1,36 @@
 function maximumUniqueSubarray(nums: number[]): number {
-  let freq = new Array(10001).fill(false);
+  let visited: { [key: string]: number } = {};
   let l = 0,
     h = 0,
     currSum = 0,
     maxSum = 0;
   while (h < nums.length) {
-    while (freq[nums[h]]) {
-      freq[nums[l]] = false;
-      currSum -= nums[l++];
+    for (let i = l; i <= visited[nums[h]]; ++i) {
+      currSum -= nums[i];
+      delete visited[nums[i]];
+        ++l;
     }
-    freq[nums[h]] = true;
+    visited[nums[h]] = h;
     currSum += nums[h++];
     maxSum = Math.max(maxSum, currSum);
   }
   return maxSum;
 }
+
+// function maximumUniqueSubarray(nums: number[]): number {
+//   let freq = new Array(10001).fill(false);
+//   let l = 0,
+//     h = 0,
+//     currSum = 0,
+//     maxSum = 0;
+//   while (h < nums.length) {
+//     while (freq[nums[h]]) {
+//       freq[nums[l]] = false;
+//       currSum -= nums[l++];
+//     }
+//     freq[nums[h]] = true;
+//     currSum += nums[h++];
+//     maxSum = Math.max(maxSum, currSum);
+//   }
+//   return maxSum;
+// }
